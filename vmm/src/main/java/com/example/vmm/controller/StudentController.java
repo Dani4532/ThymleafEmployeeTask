056b22fc782ab74c850c87d332469a541735a43d
Dani4532 <d.teszar@htlstp.at>
2022-10-03 08:47:06 +0200

    Fixed loader and detailPage.html

--- vmm/src/main/java/com/example/vmm/controller/StudentController.java
+++ vmm/src/main/java/com/example/vmm/controller/StudentController.java
@@ -1,5 +1,6 @@
 package com.example.vmm.controller;
 
+import com.example.vmm.entity.Grade;
 import com.example.vmm.loader.GradeLoader;
 import com.example.vmm.repository.GradeRepository;
 import com.example.vmm.repository.StudentRepository;
@@ -7,8 +8,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.io.IOException;
+import java.time.LocalDate;
 
 @Controller
 public class StudentController{
@@ -26,9 +29,18 @@ public class StudentController{
 
 
     @GetMapping("/students")
-    public String getAllStudents(Model model) throws IOException, InterruptedException {
+    public String getAllStudents(@RequestParam(name = "studentId", required = false) String studentId,@RequestParam(name = "subject", required = false) String subject, @RequestParam(name = "radio", required = false) Integer grade, @RequestParam(name = "date", required = false) String date, Model model) throws IOException, InterruptedException {
         if (studentRepository.findAll().get(1).getGrades().isEmpty()) {
             gradeLoader.loadGrades();
+        }
+        if (!studentId.isBlank()){
+            var student = studentRepository.findById(studentId).get();
+            var grades = student.getGrades();
+            var newGrade = new Grade(null, studentId, subject, LocalDate.parse(date), grade);
+            gradeRepository.save(newGrade);
+            grades.add(newGrade);
+            student.setGrades(grades);
+            studentRepository.save(student);
         }
         model.addAttribute("content", studentRepository.findAllSorted());
         return "students";
