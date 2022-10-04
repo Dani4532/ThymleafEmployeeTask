38fcfd70efc1504ca4101fc2aee1e4d5e6fbe7da
Dani4532 <d.teszar@htlstp.at>
2022-10-04 09:55:56 +0200

    del vmm

--- vmm/src/main/java/com/example/vmm/repository/GradeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.vmm.repository;
-
-import com.example.vmm.entity.Grade;
-import com.example.vmm.entity.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GradeRepository extends JpaRepository<Grade, Integer> {
-}
--- vmm/src/main/resources/data.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO students (id, last_name, first_name)
-VALUES ('20200475', 'Birgit', 'Bauer');
-INSERT INTO students (id, last_name, first_name)
-VALUES ('20200473', 'Adler', 'Alfred');
-INSERT INTO students (id, last_name, first_name)
-VALUES ('20200476', 'Claudia', 'Cipic');
